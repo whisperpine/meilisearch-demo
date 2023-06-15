b98d0a21d3d74755e932d7f117cb2b229469cda5
whisperpine <yusong.lai@icloud.com>
2023-06-16 03:54:36 +0800

    Deserialize json and send data to meili server

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
+use anyhow::Result;
 use serde::{Deserialize, Serialize};
 
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> Result<()> {
+    write_data().await?;
+    Ok(())
+}
+
+/// Read json file from local dir, and send data to meili server.
+pub async fn write_data() -> Result<()> {
+    use meilisearch_sdk::client::Client;
+    use tokio::fs;
+
+    let client = Client::new("http://localhost:7700", Some("master-key"));
+    let content = fs::read_to_string("movies.json").await?;
+    let movies_docs: Vec<Movie> = serde_json::from_str(&content)?;
+
+    client
+        .index("movies")
+        .add_documents(&movies_docs, Some("id"))
+        .await?;
+
+    Ok(())
 }
 
 #[derive(Serialize, Deserialize)]
