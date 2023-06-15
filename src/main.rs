763ed143216fbdcb48d16a92c006227db833564f
whisperpine <yusong.lai@icloud.com>
2023-06-16 04:39:58 +0800

    add tracing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,31 @@
 use anyhow::Result;
 use serde::{Deserialize, Serialize};
 
+const CARGO_PKG_NAME: &str = env!("CARGO_PKG_NAME");
+const CARGO_PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    write_data().await?;
+    println!("CARGO_PKG_NAME: {}", CARGO_PKG_NAME);
+    init_tracing().await;
+    // write_data().await?;
     Ok(())
+}
+
+async fn init_tracing() {
+    use tracing_subscriber::layer::SubscriberExt;
+    use tracing_subscriber::util::SubscriberInitExt;
+
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    tracing::info!("CARGO_PKG_NAME: {}", CARGO_PKG_NAME);
+    tracing::info!("CARGO_PKG_VERSION: {}", CARGO_PKG_VERSION);
 }
 
 /// Read json file from local dir, and send data to meili server.
@@ -16,10 +37,12 @@ pub async fn write_data() -> Result<()> {
     let content = fs::read_to_string("movies.json").await?;
     let movies_docs: Vec<Movie> = serde_json::from_str(&content)?;
 
-    client
+    let task_info = client
         .index("movies")
         .add_documents(&movies_docs, Some("id"))
         .await?;
+
+    tracing::debug!(?task_info);
 
     Ok(())
 }
